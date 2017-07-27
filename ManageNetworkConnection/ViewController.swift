b788d2d91706d32276324b1e6d11d53e65e0f8ae
capptan <capptanapps@gmail.com>
2017-07-27 19:02:17 -0300

    atualicoes

--- ManageNetworkConnection/ViewController.swift
+++ ManageNetworkConnection/ViewController.swift
@@ -39,41 +39,6 @@ class ViewController: NSViewController {
 		}
 	}
 
-	@discardableResult
-	func shell(_ args: String...) -> Int32 {
-		let task = Process()
-		task.launchPath = "/usr/bin/env"
-		task.arguments = args
-		task.launch()
-		task.waitUntilExit()
-		return task.terminationStatus
-	}
-	
-	func shellBash(launchPath: String, arguments: [String]) -> String
-	{
-		let task = Process()
-		task.launchPath = launchPath
-		task.arguments = arguments
-		
-		let pipe = Pipe()
-		task.standardOutput = pipe
-		task.launch()
-		
-		let data = pipe.fileHandleForReading.readDataToEndOfFile()
-		let output = String(data: data, encoding: String.Encoding.utf8)!
-		if output.characters.count > 0 {
-			//remove newline character.
-			let lastIndex = output.index(before: output.endIndex)
-			return output[output.startIndex ..< lastIndex]
-		}
-		return output
-	}
-	
-	func bash(command: String, arguments: [String]) -> String {
-		let whichPathForCommand = shellBash(launchPath: "/usr/bin/env", arguments: ["\(command)" ])
-		return shellBash(launchPath: whichPathForCommand, arguments: arguments)
-	}
-	
     private func getUserData() -> UserData? {
         guard let user = realm.objects(UserData.self).first else {
             return nil
@@ -143,10 +108,10 @@ class ViewController: NSViewController {
     @IBAction func buttonModeTapped(button: NSButton) {
         guard let userData = getUserData() else { return }
         if userData.hasCreated == false {
-            shell("sudo", "-S", "<<<", "\(userData.rootPassword)", "networksetup", "-createlocation", "AirplaneMode", "populate")
-            shell("sudo", "-S", "<<<", "\(userData.rootPassword)", "networksetup", "-switchtolocation", "AirplaneMode")
-            shell("sudo", "-S", "<<<", "\(userData.rootPassword)", "networksetup", "-setv4off", "Ethernet")
-            shell("sudo", "-S", "<<<", "\(userData.rootPassword)", "networksetup", "-switchtolocation", "\(userData.defaultLocationName)")
+			NSAppleScript(source: "do shell script \"networksetup -createlocation AirplaneMode populate\" with administrator privileges password \"\(userData.rootPassword)\"")?.executeAndReturnError(nil)
+			NSAppleScript(source: "do shell script \"networksetup -switchtolocation AirplaneMode\" with administrator privileges password \"\(userData.rootPassword)\"")?.executeAndReturnError(nil)
+			NSAppleScript(source: "do shell script \"networksetup -setv4off Ethernet\" with administrator privileges password \"\(userData.rootPassword)\"")?.executeAndReturnError(nil)
+			NSAppleScript(source: "do shell script \"networksetup -switchtolocation \(userData.defaultLocationName)\" with administrator privileges password \"\(userData.rootPassword)\"")?.executeAndReturnError(nil)
             try! realm.write {
                 userData.hasCreated = true
                 try! realm.commitWrite()
@@ -154,18 +119,21 @@ class ViewController: NSViewController {
         } else {
             let answer = dialogOKCancel(question: "You already create this location, do you want continue?", text: "Choose your answer.")
             if answer == true {
-                shell("sudo", "-S", "<<<", "\(userData.rootPassword)", "networksetup", "-createlocation", "AirplaneMode", "populate")
-                shell("sudo", "-S", "<<<", "\(userData.rootPassword)", "networksetup", "-switchtolocation", "AirplaneMode")
-                shell("sudo", "-S", "<<<", "\(userData.rootPassword)", "networksetup", "-setv4off", "Ethernet")
-                shell("sudo", "-S", "<<<", "\(userData.rootPassword)", "networksetup", "-switchtolocation", "\(userData.defaultLocationName)")
-                try! realm.write {
-                    userData.hasCreated = true
-                    try! realm.commitWrite()
-                }
+				NSAppleScript(source: "do shell script \"networksetup -createlocation AirplaneMode populate\" with administrator privileges password \"\(userData.rootPassword)\"")?.executeAndReturnError(nil)
+				NSAppleScript(source: "do shell script \"networksetup -switchtolocation AirplaneMode\" with administrator privileges password \"\(userData.rootPassword)\"")?.executeAndReturnError(nil)
+				NSAppleScript(source: "do shell script \"networksetup -setv4off Ethernet\" with administrator privileges password \"\(userData.rootPassword)\"")?.executeAndReturnError(nil)
+				NSAppleScript(source: "do shell script \"networksetup -switchtolocation \(userData.defaultLocationName)\" with administrator privileges password \"\(userData.rootPassword)\"")?.executeAndReturnError(nil)
+				try! realm.write {
+					userData.hasCreated = true
+					try! realm.commitWrite()
+				}
             }
         }
     }
 
+	@IBAction func teste(_ sender: Any) {
+		print("teste")
+	}
 }
 
 //MARK: - Structs
